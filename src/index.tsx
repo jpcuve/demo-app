4b106a5dc0eca0c6e6762db7795c09c5c169e483
Jean-Pierre <jpcuvelliez@gmail.com>
2020-04-30 15:59:40 +0200

    .

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,25 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { RouteComponentProps } from 'react-router';
 import client from './remote';
-import { createStore, Reducer, Action } from 'redux';
+import { createStore, Reducer, Action, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
 
 export interface PageProps extends RouteComponentProps {}
 
-client.get('/master/all-currency-groups').then((res: any) => console.log(JSON.stringify(res)))
-const reducer: Reducer<any, Action> = (state, action: Action) => {
-  
+export interface ApplicationState {
+  counter: number,
 }
-const store = createStore(reducer)
+
+client.get('/master/all-currency-groups').then((res: any) => console.log(JSON.stringify(res)))
+const rootReducer: Reducer<ApplicationState, Action> = (state: ApplicationState = {counter: 0}, action: Action) => {
+  console.log(`User reducer is called, state: ${JSON.stringify(state)} action: ${JSON.stringify(action)}`)
+  switch(action.type){
+    case 'increment-counter':
+      return {...state, counter: state.counter + 1}
+  }
+  return state
+}
+const store = createStore(rootReducer)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import Outline from './Outline'
-import { PageProps } from '..'
+import { PageProps, ApplicationState } from '..'
+import { useSelector, useDispatch } from 'react-redux'
 
 
 const HomePage: React.FC<PageProps> = props => {
     const [fetching, setFetching] = React.useState<boolean>(false)
     const [errors, setErrors] = React.useState<string[]>([])
-    const [count, setCount] = React.useState<number>(0)
+    const count: any = useSelector<ApplicationState>(state => state.counter)
+    const dispatch = useDispatch()
     console.debug(`Count: ${count}`)
     return (
         <Outline fetching={fetching} errors={errors} {...props}>
             <div>Home page...</div>
             <div>Count: {count}</div>
-            <button onClick={() => setCount(count + 1)}>Increment</button>
+            <button onClick={() => dispatch({type: 'increment-counter'})}>Increment</button>
         </Outline>
     );
 }
