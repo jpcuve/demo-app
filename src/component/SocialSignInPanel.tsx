b0c1b6bd58fabbdeb060d75128f451e4ca8e5236
Jean-Pierre <jpcuvelliez@gmail.com>
2020-05-08 21:36:11 +0200

    .

--- src/component/SocialSignInPanel.tsx
+++ src/component/SocialSignInPanel.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import { getApi } from '../api'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { ApplicationState } from '../store'
 declare const gapi: any
 
 const SocialSignInPanel: React.FC<{}> = () => {
   const api = getApi(useDispatch())
+  const token = useSelector<ApplicationState, string>(state => state.token)
   const onSignIn = async (googleUser: any) => {
     const profile = googleUser.getBasicProfile()
     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
     console.log('Name: ' + profile.getName());
     console.log('Image URL: ' + profile.getImageUrl());
     console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    const token = googleUser.getAuthResponse().id_token
-    console.log(`Google user token: ${token}`)
-    await api.socialSignIn('google', token)
+    const identity = googleUser.getAuthResponse().id_token
+    console.log(`Google user token: ${identity}`)
+    await api.socialSignIn('google', identity)
   }
   const signOut = async () => {
     const auth2 = gapi.auth2.getAuthInstance()
@@ -21,15 +23,17 @@ const SocialSignInPanel: React.FC<{}> = () => {
     console.log('User signed out.')
   }
   React.useEffect(() => {
-    console.log(`Setting-up google sign-in button`)
-    gapi.signin2.render('google-sign-in', {
-      'scope': 'profile email',
-      'longtitle': true,
-      'theme': 'dark',
-      'onsuccess': onSignIn,
-      // 'onfailure': props.onFailure
-    })
-  }, [])
+    if (!token){
+      console.log(`Setting-up google sign-in button`)
+      gapi.signin2.render('google-sign-in', {
+        'scope': 'profile email',
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSignIn,
+        // 'onfailure': props.onFailure
+      })
+    }  
+  }, [token])
   return (
     <div>
       <div id="google-sign-in"></div>
