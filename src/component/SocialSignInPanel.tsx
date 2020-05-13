a43be5fa0c9ecb3aeb021e9b1b0284b04215fa3b
Jean-Pierre <jpcuvelliez@gmail.com>
2020-05-13 13:08:28 +0200

    .

--- src/component/SocialSignInPanel.tsx
+++ src/component/SocialSignInPanel.tsx
@@ -9,11 +9,6 @@ const SocialSignInPanel: React.FC<FormProps> = props => {
   const api = getApi(useDispatch())
   const token = useSelector<ApplicationState, string>(state => state.token)
   const onSignIn = async (googleUser: any) => {
-    const profile = googleUser.getBasicProfile()
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     const identity = googleUser.getAuthResponse().id_token
     console.log(`Google user token: ${identity}`)
     await api.socialSignIn('google', identity)
